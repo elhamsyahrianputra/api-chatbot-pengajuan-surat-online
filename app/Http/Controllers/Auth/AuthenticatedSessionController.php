91d5dd0c656ad0cbb77a3861a5e446d425b1a071
elhamsyahrianputra <elhamsyahrianputra220402@gmail.com>
2025-04-14 03:24:41 +0800

    feat: change logout session to token base

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,14 +32,12 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request): JsonResponse
     {
-        Auth::guard('web')->logout();
+        $request->user()->currentAccessToken()->delete();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return response()->json([
+            'message' => 'Logout successfully',
+        ]);
     }
 }
